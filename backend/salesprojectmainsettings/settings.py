7faf82bc96d5141b859f7c43e6e5172c93e28ca2
mohammedreyaan177- <mohammedreyaan177@gmail.com>
2026-09-17 09:05:22 +0530

    Batch file added

--- backend/salesprojectmainsettings/settings.py
+++ backend/salesprojectmainsettings/settings.py
@@ -8,11 +8,11 @@ load_dotenv(verbose=True)
 
 
 #DB env variable injection
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_NAME = os.getenv('DB_NAME')
-DB_PORT = os.getenv('DB_PORT')
+DB_USER = "postgres"
+DB_PASSWORD = "reyaan2007"
+DB_HOST = "localhost"
+DB_NAME = "finalproject"
+DB_PORT = 5432
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
